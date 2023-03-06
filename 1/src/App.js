f7c6b346bc843cc1e603b4ee5fb2acab46d57074
chand babu <jafri.nbj@gmail.com>
2023-03-06 22:54:45 +0530

    hh

---
+++ 1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//main aap
+function App() {
+  return (
+    <div className="App">
+      <h1>I am heading one... from first component</h1>
+      <h2> am heading two... from second component</h2>
+      <h3> am heading three... from third component</h3>
+      <button>Click Me !!</button>
+      </div>
+  );
+}
+
+export default App;

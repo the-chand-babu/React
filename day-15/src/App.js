3ad6f3eb630065c145624746a1aef6e546d5b76b
chand babu <jafri.nbj@gmail.com>
2023-04-10 17:53:14 +0530

    question 2

--- day-15/src/App.js
+++ day-15/src/App.js
@@ -1,28 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(2);
-  let Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // "2": [
+  //   "create a h1",
+  //   "create a h2",
+  //   "create a div",
+  //   "manage state to track that when user mouse overed on the h1 and when mouse out with date and time",
+  //   "create a html table to show the logs.",
+  //   [
+  //     { "time": "", "eventName": "out or in" },
+  //     { "time": "", "eventName": "out or in" }
+  //   ]
+  // ],
 
-  const hanelupdate = () => {
-    const value = Math.floor(Math.random() * 100 + 1);
-    setCount(value);
+  // const [isover,setIsover]=useState(false);
+  const [data, setData] = useState([]);
+
+  const handelmouseoverh1 = () => {
+    const date = new Date();
+    const time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    const d = new Date();
+    const dat = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
+
+    // setIsover(true);
+    setData([
+      ...data,
+      {
+        time: time,
+        date: dt,
+        eventName: "in",
+        tag: "h1",
+      },
+    ]);
   };
 
-  return (
-    <div className='App'>
-      <h1>Table Generator !</h1>
-      <ul>
-        {Array.map((ele) => {
-          return <li>{count * ele}</li>;
-        })}
-      </ul>
+  const handlemouseOut = () => {
+    const date = new Date();
+    const time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
-      <button onClick={hanelupdate}>Update</button>
+    const d = new Date();
+    const dat = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
+
+    // setIsover(false)
+    setData([
+      ...data,
+      {
+        time: time,
+        date: dt,
+        eventName: "out",
+        tag: "h1",
+      },
+    ]);
+  };
+
+  //h2 function  ....
+
+  const handelmouseoverh2 = () => {
+    const date = new Date();
+    const time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    // setIsover(true);
+
+    const d = new Date();
+    const dat = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
+
+    setData([
+      ...data,
+      {
+        time: time,
+        date: dt,
+        eventName: "in",
+        tag: "h2",
+      },
+    ]);
+  };
+
+  const handlemouseOut2 = () => {
+    const date = new Date();
+    const time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    // setIsover(false);
+
+    const d = new Date();
+    const dat = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
+
+    setData([
+      ...data,
+      {
+        time: time,
+        date: dt,
+        eventName: "out",
+        tag: "h2",
+      },
+    ]);
+  };
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      <h1 onMouseOverCapture={handelmouseoverh1} onMouseLeave={handlemouseOut}>
+        I am H1!
+      </h1>
+      <h2 onMouseOverCapture={handelmouseoverh2} onMouseLeave={handlemouseOut2}>
+        I am H2!
+      </h2>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>time</th>
+
+              <th>Event Name</th>
+              <th>Tag Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((ele) => {
+              return (
+                <tr>
+                  <td>{ele.date}</td>
+
+                  <td>{ele.time}</td>
+                  <td>{ele.eventName}</td>
+                  <td>{ele.tag}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;

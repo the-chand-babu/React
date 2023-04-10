7e5b5bb79db26ba1a24350962762a875955ac34e
chand babu <jafri.nbj@gmail.com>
2023-04-10 18:11:59 +0530

    question3

--- day-15/src/App.js
+++ day-15/src/App.js
@@ -3,152 +3,28 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // "2": [
-  //   "create a h1",
-  //   "create a h2",
-  //   "create a div",
-  //   "manage state to track that when user mouse overed on the h1 and when mouse out with date and time",
-  //   "create a html table to show the logs.",
-  //   [
-  //     { "time": "", "eventName": "out or in" },
-  //     { "time": "", "eventName": "out or in" }
-  //   ]
-  // ],
+   //   3": [
+  //     "create an array with multiple greetings",
+  //     "create a button and handle click on button ",
+  //     "show the next greeting message on h1"
+  //   ]
 
-  // const [isover,setIsover]=useState(false);
-  const [data, setData] = useState([]);
+  const [count, SetCount] = useState(0);
 
-  const handelmouseoverh1 = () => {
-    const date = new Date();
-    const time =
-      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  let array = ['Happy aniversary ', 'Happy Birthday Rohit', 'happy new year', 'happy diwali', 'happy eid', 'happy indepdence day'];
 
-    const d = new Date();
-    const dat = d.getDate();
-    const month = d.getMonth();
-    const year = d.getFullYear();
-    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
-
-    // setIsover(true);
-    setData([
-      ...data,
-      {
-        time: time,
-        date: dt,
-        eventName: "in",
-        tag: "h1",
-      },
-    ]);
-  };
-
-  const handlemouseOut = () => {
-    const date = new Date();
-    const time =
-      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-    const d = new Date();
-    const dat = d.getDate();
-    const month = d.getMonth();
-    const year = d.getFullYear();
-    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
-
-    // setIsover(false)
-    setData([
-      ...data,
-      {
-        time: time,
-        date: dt,
-        eventName: "out",
-        tag: "h1",
-      },
-    ]);
-  };
-
-  //h2 function  ....
-
-  const handelmouseoverh2 = () => {
-    const date = new Date();
-    const time =
-      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-    // setIsover(true);
-
-    const d = new Date();
-    const dat = d.getDate();
-    const month = d.getMonth();
-    const year = d.getFullYear();
-    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
-
-    setData([
-      ...data,
-      {
-        time: time,
-        date: dt,
-        eventName: "in",
-        tag: "h2",
-      },
-    ]);
-  };
-
-  const handlemouseOut2 = () => {
-    const date = new Date();
-    const time =
-      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    // setIsover(false);
-
-    const d = new Date();
-    const dat = d.getDate();
-    const month = d.getMonth();
-    const year = d.getFullYear();
-    let dt = dat + "-" + 0 + (month + 1) + "-" + year;
-
-    setData([
-      ...data,
-      {
-        time: time,
-        date: dt,
-        eventName: "out",
-        tag: "h2",
-      },
-    ]);
-  };
-
-  console.log(data);
+  function handleCount() {
+    if (count < array.length - 1) {
+      SetCount(count + 1);
+    } else {
+      SetCount(0);
+    }
+  }
 
   return (
     <div className="App">
-      <h1 onMouseOverCapture={handelmouseoverh1} onMouseLeave={handlemouseOut}>
-        I am H1!
-      </h1>
-      <h2 onMouseOverCapture={handelmouseoverh2} onMouseLeave={handlemouseOut2}>
-        I am H2!
-      </h2>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>time</th>
-
-              <th>Event Name</th>
-              <th>Tag Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((ele) => {
-              return (
-                <tr>
-                  <td>{ele.date}</td>
-
-                  <td>{ele.time}</td>
-                  <td>{ele.eventName}</td>
-                  <td>{ele.tag}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <h1>{array[count]}</h1>
+      <button onClick={handleCount}>Next Greeting</button>
     </div>
   );
 }
